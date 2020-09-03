295ea29d2c4e5d17e1ba89108091884fb29aee57
Gana-Lazare <autolazare@gmail.com>
2020-09-02 22:40:43 -0400

    ebaytest

--- Generic/src/main/java/common/WebAPI.java
+++ Generic/src/main/java/common/WebAPI.java
@@ -1,9 +1,7 @@
 package common;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
